4a2216d50d9948fa119837f99280da677f763cea
reasonsx <kamkre01@easv365.dk>
2024-02-26 09:34:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DD-w9-JS-exercise
+ 
---
+++ js/script.js
@@ -0,0 +1,6 @@
+const myArray = ["apple", "banana", "cherry", "grape"];
+
+// 1. Using forEach with an array
+myArray.forEach(item => {
+    console.log(item);
+})
